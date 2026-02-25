2252daf765da4fbcd92165ab2367dfb8de9fd0a4
Sergio Guadarrama <sguadarrama@tiprotec.com>
2026-02-25 17:29:27 -0600

    chore: update gitignore for build and docusaurus

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("C:\\Users\\Sergio\\Documents\\dev\\architect-linter-pro\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\Users\\Sergio\\Documents\\dev\\architect-linter-pro\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("C:\\Users\\Sergio\\Documents\\dev\\architect-linter-pro\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("C:\\Users\\Sergio\\Documents\\dev\\architect-linter-pro\\src\\css\\custom.css"),
-];
--- build/assets/js/aba21aa0.7db2b1c1.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkarchitect_linter_pro_docs=self.webpackChunkarchitect_linter_pro_docs||[]).push([[742],{7093(e){e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
